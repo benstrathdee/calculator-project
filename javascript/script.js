79ae634c66806a0f83e0cb9cdca26f98486d65a9
Ben Strathdee <ben.strathdee@live.com.au>
2021-11-16 15:12:10 +1100

    added basic project breakdown

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+// Display:
+// A large element to act as a display, show current input and result
+
+// First line:
+// A function for turning the display on or if display is on, set ALL input/memory/result variables to 0 (ON/AC button)
+// A function to turn off display and set everything to 0 (OFF button)
+// A non-functional music button? Maybe make it do a cute alert
+
+// Second line:
+// A variable to contain a value in memory
+// A function to store something in memory (the MS button)
+// A function to clear memory (MC button)
+// A function to enter the memory value in the equation (MR button)
+// A function to add memory value to current input (M+ button)
+// A function to subtract memory value from current input (M- button)
+
+// Numbers block:
+// A single function that takes pressed button and inputs it into the input variable (0-9 + . buttons, +/-/ / /*/%/+-/sqrt)
+// A function that sets input variable to empty (C button)
+// A function that calculates the result from the input values (= button)
+
+// Backend:
+// A variable to store current input (to be displayed)
+// A variable to store result (to be displayed)
+// A variable to store something in memory (for the M buttons)
